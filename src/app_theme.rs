f0763b56c2024c77a22b6ad218ada29bfa83e37b
Benjamin Mapp <rebo@Benjamins-MacBook-Pro.local>
2020-05-10 14:26:12 +0100

    initial commit

---
+++ src/app_theme.rs
@@ -0,0 +1,141 @@
+use seed_style::measures::px;
+use seed_style::*;
+// Theme Definition
+// -----------------
+//
+// A Theme Object is where all css related values and presets can be stored
+// and then accessed at any point in the view.
+//
+// The Theme Object is broadly consistent with the Theme specification that is used in
+// several css librarys: https://theme-ui.com/theme-spec/
+//
+// A Theme object is made up of named css values called aliases
+// as well as scales for css values/
+//
+// Having a scale is useful for things like sizes and spacing
+// because you can have consistent layout throughout your app.  For instance pixel gaps
+// at 4px increments.
+//
+// Having named aliases for things like colors is useful because it means
+// swapping out colors, or having a dark/light theme can be defined in a central location.
+//
+// In order to use cssvalue aliases we use an enum.
+//
+// // Main Color Theme Keys
+#[derive(Hash, PartialEq, Eq, Clone)]
+pub enum Color {
+    Background,
+    MainText,
+    Primary,
+    MutedPrimary,
+    DarkPrimary,
+    MutedSecondary,
+    Secondary,
+    DarkSecondary,
+    Highlight,
+}
+impl ColorTheme for Color {} // Allows you to use a `Color` variant as a CssColor alias in the theme.
+
+// Named Breakpoints Keys allow you to refer to a named breakpoint in layout helpers and css media queries.
+#[derive(Hash, PartialEq, Eq, Clone, Debug)]
+pub enum Breakpoint {
+    ExtraSmall,
+    Small,
+    Medium,
+    Large,
+    ExtraLarge,
+}
+impl BreakpointTheme for Breakpoint {} // Enable `Breakpoint` as a Breakpoint alias.
+
+// The good thing about declaring the keys ahread of time is that it is easy for
+// A user of the app / component to know what can be set to configure the style of the app
+
+// WIth the keys declared, we can now actually define a theme that we want to use:
+// We can have multiple themes for a single Theme definition if needed.
+
+// The following value classes are themeable with named aliases:
+//
+//  BorderTheme,
+//  BorderWidthTheme,
+//  BorderStyleTheme,
+//  SpaceTheme,
+//  LineHeightTheme,
+//  LetterSpacingTheme,
+//  BorderRadiusTheme,
+//  FontTheme,
+//  FontSizeTheme,
+//  SizeTheme,
+//  TransitionTheme,
+//  ZIndexTheme,
+//  DisplayTheme,
+//  ColorTheme,
+//  ShadowTheme,
+//  StyleTheme,
+//  BreakpointTheme,
+
+// We now write a function to provide an instance of a theme.
+// A different function could provide a completely different theme
+// For instance a dark mode theme.
+
+pub fn theme() -> Theme {
+    use Breakpoint::*;
+
+    // I generally set the named aliases seperately from the theme scales:
+    let theme = Theme::new()
+        // Various theme colors
+        .set_color(Color::Background, CssColor::Hex(0xFFFFFF))
+        .set_color(Color::MainText, CssColor::Hex(0x000000))
+        .set_color(Color::Primary, CssColor::Hsl(100.0, 70.0, 40.0))
+        .set_color(Color::MutedPrimary, CssColor::Hsl(100.0, 50.0, 50.0))
+        .set_color(Color::Secondary, CssColor::Hex(0xBBBBBB)) // or use the hsl shortcut
+        .set_color(Color::MutedSecondary, CssColor::Hex(0xDDDDDD)) // or use the hsl shortcut
+        .set_color(Color::Highlight, hsl(310, 70, 85))
+        .set_color(Color::DarkPrimary, hsl(200, 70, 35))
+        .set_color(Color::DarkSecondary, hsl(300, 60, 20))
+        // set named breakpoints for conditional rendering
+        .set_breakpoint(ExtraSmall, (0, Some(600))) // Breakpoints are upper bound exclusive lower bound inclusive.
+        .set_breakpoint(Small, (600, Some(960)))
+        .set_breakpoint(Medium, (960, Some(1280)))
+        .set_breakpoint(Large, (1280, Some(1920)))
+        .set_breakpoint(ExtraLarge, (1920, None));
+
+    // other aliases you can set include :
+    // color, space, size, font_size, font, border, border_width, border_style, border_radius, transition
+    // line_height, letter_spacing,
+
+    //scales
+    // https://styled-system.com/guides/array-scales/
+    let theme = theme
+        .border_width_scale(&[px(2), px(4), px(8), px(12), px(16), px(24), px(32)])
+        .space_scale(&[px(2), px(4), px(8), px(12), px(16), px(24), px(32)])
+        .size_scale(&[px(1), px(2), px(4)])
+        .font_size_scale(&[px(14), px(18), px(20), px(36)])
+        // Other scales you can set are :
+        // sizes_scale, spaces_scale, borders_scale, font_sizes_scale, border_widths_scale, border_styles_scale, radii_scale, colors_scale, shadows_scale,
+        //
+        // Breakpoint scales for 'in-property' setting of values that are responsive to breakpoints
+        .breakpoint_scale([600, 960, 1280, 1920]); // standard-material-ui breakpoints
+
+    // Global styles to apply to any selectors you choose based on theme.
+    // When a theme is changed new global styles can be used.
+    theme.set_global_styles(
+        GlobalStyle::default()
+            .style("p", s().padding(px(24)))
+            .style(
+                "a,div,p,h1,h2,h3,h4,li,dd,dt,button,label,input",
+                s().font_family("'Lato',sans-serif")
+                    .webkit_font_smoothing_antialiased(),
+            )
+            .style("h1", s().mb(px(24)).padding(px(18)))
+            .style(
+                "a,div,p,h1,h2,h3,h4,li,dd,dt,button,label,input",
+                s().font_family("Roboto,sans-serif")
+                    .webkit_font_smoothing_antialiased(),
+            )
+            .style(
+                "body",
+                s().bg_color(CssColor::Hex(0xBBBBBB))
+                    .color(CssColor::Hex(0x000000)),
+            ),
+    )
+}
---
+++ src/lib.rs
@@ -0,0 +1,160 @@
+#![feature(track_caller)]
+use seed::prelude::*;
+use seed::*;
+
+use seed_hooks::*;
+use seed_style::measures::{pc, px};
+use seed_style::*;
+
+mod app_theme;
+use app_theme::*;
+
+// This app shows how to use most features of a proposed styling system for Seed.
+//
+// It includes :
+//
+// Component Style - Define CSS styles for html elements.
+// Theming Support -Define themes for your app so common styles can set centrally
+// Layout - Declare Application layout ahead of time in a context agnostic way.
+
+// Not all of these need to be used, all features are optional.
+//
+//  Model, Msg, Update, init(), and start()
+//  ---------------------------------------
+
+pub struct Model {}
+
+// In aps that make use of conditional rendering on breakpoints we We just need one Msg
+// in order to handle a WindowResized event.
+#[derive(Clone)]
+pub enum Msg {
+    WindowResized,
+}
+
+// Update optionally hamdles WindowResized. For performance reasons we dont want to
+// re-render the app on every window resize, only if the resize takes the window into new breakpoint
+// this step could be completely left off and just added in at the end of a design once all breakpoints have been
+// firmly decided.
+fn update(msg: Msg, _model: &mut Model, orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::WindowResized => {
+            // We just need to provide a copy of the theme that is providing the breakpoints.
+            // it is passed in a block because we only need it on first assignment.
+            seed_style::conditionally_skip_rendering::<app_theme::Breakpoint, _, _, _>(
+                || app_theme::theme(),
+                orders,
+            )
+        }
+    }
+}
+
+// We subscribe to a window resize event in the init in order to handle window resizing
+fn init(_url: Url, orders: &mut impl Orders<Msg>) -> Model {
+    // Global style resets above normalize.css
+    // Mostly box-sizing and global font.
+    // .activate_init_styles should be called only once
+    GlobalStyle::default()
+        // ensure all buttons are centred by default and outline free
+        .style("button", s().align_self_center().outline_style_none())
+        //ensure box-sizing: border-box() for everything
+        .style("html", s().box_sizing_border_box())
+        .style("img", s().box_sizing_content_box())
+        .style("*, *:before, *:after", s().box_sizing("inherit"))
+        .activate_init_styles();
+
+    // Setup a stream of Resize events.
+    orders.stream(streams::window_event(Ev::Resize, |_| Msg::WindowResized));
+
+    Model {}
+}
+
+// Default app start...
+#[wasm_bindgen(start)]
+pub fn start() {
+    App::start("app", init, update, view);
+}
+
+//  View Entry Here, Sets up theme access, two themes are allowed access
+//
+//  The first is the app defined theme, the second provides access to seed style presets.
+//  (At present `Theme` is not Clone therefore need to pass in as owned Vec)  to be improved in future.
+//
+//  ---------------
+#[topo::nested] // Needed for Seed Hooks
+pub fn view(model: &Model) -> Node<Msg> {
+    // ensure our app is themed, include style presets theme.
+    use_themes(
+        || vec![app_theme::theme(), seed_style::style_presets()],
+        || themed_view(model),
+    )
+}
+
+pub fn themed_view(_model: &Model) -> Node<Msg> {
+    let counter = use_state(|| 0);
+    div![
+        s().display_flex()
+            .flex_direction_row()
+            .justify_content_center()
+            .align_items_center()
+            .height(pc(100)),
+        div![
+            s().background_color(seed_colors::Gray::No2)
+                .margin_x(px(80))
+                .radius(px(4))
+                .min_height(px(200))
+                .display_flex()
+                .flex_direction_column()
+                .padding(&[px(24), px(32), px(48)]),
+            h1![
+                s().border_bottom_width(px(4))
+                    .border_bottom_style_solid()
+                    .justify_self_center()
+                    .border_bottom_color(Color::Primary)
+                    .font_weight_v700()
+                    .font_size(&[px(18), px(24), px(32)]),
+                "Seed Rocks!"
+            ],
+            p![
+                s().font_size(&[px(12), px(14), px(18)]),
+                "The contents of this div are reactive, try shrinking the browser."
+            ],
+            p![
+                s().font_size(&[px(12), px(14), px(18)]),
+                "Also try clicking this button! You have clicked it ",
+                counter,
+                " time(s)"
+            ],
+            button![
+                s().bg_color(Color::MutedPrimary)
+                    .radius(px(5))
+                    .my(px(20))
+                    .justify_self_center()
+                    .padding_x(&[px(18), px(24)])
+                    .padding_y(&[px(8), px(12)]),
+                s().hover().bg_color(Color::Primary),
+                s().active().bg_color(Color::DarkPrimary),
+                "Click Me",
+                counter.on_click(|c| *c += 1)
+            ],
+            only_and_above(Breakpoint::Medium, || {
+                let hidden_counter = use_state(|| 0);
+                div![
+                    s().display_flex().flex_direction_column(),
+                p![
+                "This is only rendered at larger Breakpoints. This is not display: hidden; it is actually conditional rendering. Notice it keeps it's count even when hidden!"],
+                button![
+                    s().bg_color(hsl(20,70,50))
+                    .radius(px(5))
+                    .my(px(20))
+                    .padding_x(&[px(18), px(24)])
+                    .padding_y(&[px(8), px(12)]),
+                s().hover().bg_color(hsl(20,80,70)),
+                s().active().bg_color(hsl(20,50,30)),
+                "Clicked ", hidden_counter, " times",
+                hidden_counter.on_click(|c| *c+=1)
+                ]
+            ]
+            })
+        ]
+    ]
+}
